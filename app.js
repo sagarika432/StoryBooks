90da944161d86638a44cffbbac8e47703888e110
varshak333 <varshak333@gmail.com>
2018-07-13 01:41:40 +0530

    final commit1

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express');
+const path = require('path');
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const passport = require('passport');
 
 
-//load User model
+//load Models
 require('./models/User');
+require('./models/Story');
 
 //Passport Config
 require('./config/passport')(passport);
@@ -16,9 +20,19 @@ require('./config/passport')(passport);
 //load routes
 const index = require('./routes/index');
 const auth = require('./routes/auth');
+const stories = require('./routes/stories');
 
 //load Keys 
 const keys  = require('./config/keys');
+
+//handlebars helpers
+const {
+    truncate,
+    stripTags,
+    formatDate,
+    select,
+    editIcon
+} = require('./helpers/hbs');
 
 //Map global promises
 mongoose.Promise = global.Promise;
@@ -35,8 +49,24 @@ mongoose.connect(keys.mongoURI ,{
 
 const app = express();
 
+
+//body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+// Method Override Middelware
+app.use(methodOverride('_method'));
+
 //Handlebars middleware
 app.engine('handlebars',exphbs({
+    helpers:{
+        truncate: truncate,
+        stripTags : stripTags,
+        formatDate: formatDate,
+        select : select,
+        editIcon : editIcon
+    },
     defaultLayout:'main'
 }));
 app.set('view engine','handlebars');
@@ -69,9 +99,14 @@ app.use((req,res,next) => {
 
 });
 
+//Set static folder
+app.use(express.static(path.join(__dirname,'public')));
+
+
 //Use routes
 app.use('/',index);
 app.use('/auth',auth);
+app.use('/stories',stories);
 
 const port = process.env.PORT || 5000;
 
