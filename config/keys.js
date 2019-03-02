749034becc1a5cd176bb15e74ad580bd394744fb
varshak333 <varshak333@gmail.com>
2019-03-02 11:12:19 +0530

    reg + login

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 module.exports ={
-    //mongoURI:'mongodb://varshak333:varshak333@ds233061.mlab.com:33061/storybooks-dev-varsha',
-    mongoURI: 'mongodb://localhost/storybooks-dev-varsha',
+    mongoURI:'mongodb://sihtigerauth:sihtigerauth2019@ds157735.mlab.com:57735/storybooks-tigerauth',
+    //mongoURI: 'mongodb://localhost/storybooks-dev-varsha',
     googleClientID:'282022218244-3d9c4h0nuv1f3327ogc3k4bhumnps8g4.apps.googleusercontent.com',
     googleClientSecret:'lUoMFruYciLHSnnzXHxlwqmO'
 }
